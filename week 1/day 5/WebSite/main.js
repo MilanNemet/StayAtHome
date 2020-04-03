fdb8062b12921f11bcb7be3714e1b3bf3c390ff8
milan.nemeth@sulid.hu <milan.nemeth@sulid.hu>
2020-04-03 18:05:25 +0200

    some kind of final

--- week 1/day 5/WebSite/main.js
+++ week 1/day 5/WebSite/main.js
@@ -1,1 +1,33 @@
 // JavaScript source code
+
+var sections;
+var links;
+
+window.onload = function () {
+
+    sections = document.querySelectorAll("section");
+    links = document.querySelectorAll("nav > a");
+
+    initLinks(links, hideElement);
+}
+
+var initLinks = function (links, hide) {
+    for (var i = 0; i < links.length; i++) {
+        links[i].addEventListener("click", hide);
+    }
+}
+
+var hideElement = function (event) {
+
+    var elementId = event.target.id;
+    var index = elementId[elementId.length - 1];
+
+    for (var i = 0; i < sections.length; i++) {
+        if (i+1 == index) {
+            sections[i].style.display = "inline-block";
+        }
+        else {
+            sections[i].style.display = "none";
+        }
+    }
+}
